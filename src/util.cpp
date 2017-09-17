68cde10fb078becae6291ff30c975031b44da6a9
sennevb <sennevanbaelen@gmail.com>
2017-09-17 19:10:18 +0200

    change dir to bitcoinz
    if you want to run zclassic and bitcoinz on same system

--- src/util.cpp
+++ src/util.cpp
@@ -465,7 +465,7 @@ boost::filesystem::path GetDefaultDataDir()
     return pathRet / "Zcash";
 #else
     // Unix
-    return pathRet / ".zcash";
+    return pathRet / ".bitcoinz";
 #endif
 #endif
 }
@@ -582,7 +582,7 @@ void ClearDatadirCache()
 
 boost::filesystem::path GetConfigFile()
 {
-    boost::filesystem::path pathConfigFile(GetArg("-conf", "zcash.conf"));
+    boost::filesystem::path pathConfigFile(GetArg("-conf", "bitcoinz.conf"));
     if (!pathConfigFile.is_complete())
         pathConfigFile = GetDataDir(false) / pathConfigFile;
 
